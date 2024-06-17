c3110acaaa52b67a3565f5cd7d799f09114d16cd
Mohamed Jebri <159800102+memedjjebri@users.noreply.github.com>
2024-06-17 17:01:54 +0200

    fixing tailwind css

--- src/components/Hireme.jsx
+++ src/components/Hireme.jsx
@@ -2,26 +2,26 @@
 
 const Hireme = () => {
   return (
-    <section id="hireme" className="py-10 px-3 text-white">
-      <div className="text-center">
-        <h3 className="text-4xl font-semibold">
-          Hire <span className="text-cyan-600">Me</span>
+    <section id="hireme" class="py-10 px-3 text-white">
+      <div class="text-center">
+        <h3 class="text-4xl font-semibold">
+          Hire <span class="text-cyan-600">Me</span>
         </h3>
-        <p className="text-gray-400 mt-3 text-lg">Do you have any work?</p>
+        <p class="text-gray-400 mt-3 text-lg">Do you have any work?</p>
       </div>
-      <div className="bg-gray-700 relative px-8 rounded-2xl py-5 lg:max-w-4xl mx-auto min-h-[24rem] mt-24 flex gap-6 lg:flex-row flex-col-reverse items-center">
+      <div class="bg-gray-700 relative px-8 rounded-2xl py-5 lg:max-w-4xl mx-auto min-h-[24rem] mt-24 flex gap-6 lg:flex-row flex-col-reverse items-center">
         <div>
-          <h2 className="text-2xl font-semibold">
+          <h2 class="text-2xl font-semibold text-white">
             Do you want any work from me?
-            <p>
-              As a Web developers, I typically work on creating and maintaining
-              websites and web applications, focusing on front-end design,
-              back-end functionality, and ensuring performance and security. I
-              use a variety of tools and technologies like HTML, CSS,
-              JavaScript, databases, and frameworks to build responsive, and
-              efficient web solutions.
-            </p>
           </h2>
+          <p class="text-gray-300 mt-3 text-lg leading-7">
+            As a web developer, I typically work on creating and maintaining
+            websites and web applications, focusing on front-end design,
+            back-end functionality, and ensuring performance and security. I use
+            a variety of tools and technologies like HTML, CSS, JavaScript,
+            databases, and frameworks to build responsive and efficient web
+            solutions.
+          </p>
         </div>
       </div>
     </section>
